1d06782b1804f5ad0868bb5128b0716972a2f046
Jan Scholtyssek <jan@janscholtyssek.dk>
2017-05-17 18:03:28 +0200

    Added CompactnessMeasurer + unit test

---
+++ ChemistryLibrary/Measurements/CompactnessMeasurer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Commons;
+using MIConvexHull;
+
+namespace ChemistryLibrary.Measurements
+{
+    /// <summary>
+    /// Measures compactness of peptide chain using convex hull
+    /// </summary>
+    public static class CompactnessMeasurer
+    {
+        public static double Measure(ApproximatePeptide peptide)
+        {
+            var aminoAcidPositions = peptide.AminoAcids
+                .SelectMany(aa => new[] {aa.NitrogenPosition, aa.CarbonAlphaPosition, aa.CarbonPosition})
+                .ToList();
+            var vertices = ToVertices(aminoAcidPositions);
+            var convexHull = ConvexHull.Create(vertices);
+            var volume = CalculateVolume(convexHull);
+            return volume;
+        }
+
+        private static double CalculateVolume(ConvexHull<Vertex3D, DefaultConvexFace<Vertex3D>> convexHull)
+        {
+            var oneThird = 1.0/3.0;
+            var volume = 0.0;
+            var origin = convexHull.Points.First().Point;
+            foreach (var face in convexHull.Faces)
+            {
+                var faceOrigin = face.Vertices[1].Point;
+                var faceVector1 = (face.Vertices[1].Point - face.Vertices[0].Point).ToVector3D();
+                var faceVector2 = (face.Vertices[2].Point - face.Vertices[1].Point).ToVector3D();
+                var faceArea = 0.5*faceVector1.CrossProduct(faceVector2).Magnitude();
+                var blockHeight = origin.DistanceFromPlane(faceOrigin, faceVector1, faceVector2);
+                var blockVolume = oneThird*faceArea*blockHeight;
+                volume += blockVolume;
+            }
+            return volume;
+        }
+
+        private class Vertex3D : IVertex
+        {
+            public Vertex3D(double x, double y, double z)
+            {
+                Position = new[] {x, y, z};
+                Point = new Point3D(x, y, z);
+            }
+
+            public double[] Position { get; }
+            public Point3D Point { get; }
+        }
+        private static IList<Vertex3D> ToVertices(List<UnitPoint3D> aminoAcidPositions)
+        {
+            return aminoAcidPositions
+                .Select(aaPosition => aaPosition.In(SIPrefix.Pico, Unit.Meter))
+                .Select(aaPosition => new Vertex3D(aaPosition.X, aaPosition.Y, aaPosition.Z))
+                .ToList();
+        }
+    }
+}
---
+++ ChemistryLibraryTest/Measurements/CompactnessMeasurerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ChemistryLibrary;
+using ChemistryLibrary.Measurements;
+using Commons;
+using NUnit.Framework;
+
+namespace ChemistryLibraryTest.Measurements
+{
+    [TestFixture]
+    public class CompactnessMeasurerTest
+    {
+        [Test]
+        public void VolumeAsExpected()
+        {
+            var aa1 = new ApproximatedAminoAcid(AminoAcidName.Alanine);
+            var aa2 = new ApproximatedAminoAcid(AminoAcidName.Alanine);
+            var aa3 = new ApproximatedAminoAcid(AminoAcidName.Alanine);
+            var peptide = new ApproximatePeptide(new List<ApproximatedAminoAcid> { aa1, aa2, aa3 });
+
+            // Set positions
+            aa1.NitrogenPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 0, 0, 0);
+            aa1.CarbonAlphaPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 1, 0, 0);
+            aa1.CarbonPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 1, 1, 0);
+            aa2.NitrogenPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 0, 1, 0);
+            aa2.CarbonAlphaPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 0, 1, 1);
+            aa2.CarbonPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 1, 1, 1);
+            aa3.NitrogenPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 1, 0, 1);
+            aa3.CarbonAlphaPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 0, 0, 1);
+            aa3.CarbonPosition = new UnitPoint3D(SIPrefix.Pico, Unit.Meter, 0.5, 0.5, 0.5);
+
+            var volume = CompactnessMeasurer.Measure(peptide);
+            Assert.That(volume, Is.EqualTo(1).Within(1e-6));
+        }
+    }
+}
