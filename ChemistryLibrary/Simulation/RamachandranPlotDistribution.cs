759aa8843c69afe8fbf5108f8caeb9b011013627
Jan Scholtyssek <jan@janscholtyssek.dk>
2017-06-25 17:28:25 +0200

    Caching RamachandranPlotDistribution plots as CSV files and reading them when available.

--- ChemistryLibrary/Simulation/RamachandranPlotDistribution.cs
+++ ChemistryLibrary/Simulation/RamachandranPlotDistribution.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using ChemistryLibrary.Extensions;
 using ChemistryLibrary.Measurements;
 using ChemistryLibrary.Objects;
 using Commons;
+using Commons.IO;
 
 namespace ChemistryLibrary.Simulation
 {
@@ -16,9 +18,46 @@ namespace ChemistryLibrary.Simulation
         {
             AminoAcidName = aminoAcidName;
             this.gridSteps = gridSteps;
-            var angles = ParseDistributionFile(distributionFilePath);
-            DistributionPlot = GenerateDistributionPlot(angles);
+            var distributionCacheFilename = GetCachedFilename(distributionFilePath);
+            if (File.Exists(distributionCacheFilename))
+            {
+                DistributionPlot = LoadDistributionGrid(distributionCacheFilename);
+            }
+            if(DistributionPlot == null 
+                || DistributionPlot.GetLength(0) != gridSteps
+                || DistributionPlot.GetLength(1) != gridSteps)
+            {
+                var angles = ParseDistributionFile(distributionFilePath);
+                DistributionPlot = GenerateDistributionPlot(angles);
+                StoreDistibutionPlot(distributionCacheFilename);
+            }
             GradientPlot = GenerateGradientPlot(DistributionPlot);
+        }
+
+        private string GetCachedFilename(string distributionFilePath)
+        {
+            var directory = Path.GetDirectoryName(distributionFilePath);
+            if (directory == null)
+            {
+                return Path.Combine("Cache", AminoAcidName.ToThreeLetterCode() + ".csv");
+            }
+            return Path.Combine(
+                directory,
+                "Cache",
+                AminoAcidName.ToThreeLetterCode() + ".csv");
+        }
+
+        private double[,] LoadDistributionGrid(string distributionCacheFilename)
+        {
+            return CsvReader.ReadDoubleArray(distributionCacheFilename);
+        }
+
+        private void StoreDistibutionPlot(string distributionCacheFilename)
+        {
+            var directory = Path.GetDirectoryName(distributionCacheFilename);
+            if (directory != null && !Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+            CsvWriter.Write(DistributionPlot, distributionCacheFilename);
         }
 
         public AminoAcidName AminoAcidName { get; }
@@ -29,22 +68,28 @@ namespace ChemistryLibrary.Simulation
         {
             var distribution = new double[gridSteps, gridSteps];
             var probabilityDensitySum = 0.0;
+            foreach (var aminoAcidAngle in aminoAcidAngles)
+            {
+                probabilityDensitySum += ApplyPointDistribution(aminoAcidAngle.Phi, aminoAcidAngle.Psi, distribution);
+            }
+            // Prohibited zone
+            var prohibitedZoneStrength = 1.0/(gridSteps*gridSteps) * probabilityDensitySum;
             for (int psiStepIdx = 0; psiStepIdx < gridSteps; psiStepIdx++)
             {
                 for (int phiStepIdx = 0; phiStepIdx < gridSteps; phiStepIdx++)
                 {
                     // Non-normalized probability density
                     var angles = GetAnglesFromGridPosition(new Point2D(phiStepIdx, psiStepIdx));
-                    var prohibitedZonePhiCenter = -30*Math.Sin(2 * angles.Psi.In(Unit.Radians));
+                    var prohibitedZonePhiCenter = -30 * Math.Sin(2 * angles.Psi.In(Unit.Radians));
                     var prohibitedZoneWidth = 60;
 
                     var phi = angles.Phi.In(Unit.Degree);
                     var distanceFromCenter = phi - prohibitedZonePhiCenter;
                     var distanceSquared = distanceFromCenter * distanceFromCenter;
                     var zoneWidthSquared = prohibitedZoneWidth * prohibitedZoneWidth;
-                    var probabilityDensity = (1.0 - Math.Exp(-distanceSquared/zoneWidthSquared));
+                    var probabilityDensity = prohibitedZoneStrength*(1.0 - Math.Exp(-distanceSquared / zoneWidthSquared));
 
-                    distribution[psiStepIdx, phiStepIdx] = probabilityDensity;
+                    distribution[psiStepIdx, phiStepIdx] += probabilityDensity;
                     probabilityDensitySum += probabilityDensity;
                 }
             }
@@ -57,6 +102,51 @@ namespace ChemistryLibrary.Simulation
                 }
             }
             return distribution;
+        }
+
+        private double ApplyPointDistribution(UnitValue phi, UnitValue psi, double[,] distribution)
+        {
+            const double Sigma = 10;
+            var sigmaSteps = gridSteps * Sigma / 360;
+            var sigmaStepsSquared = sigmaSteps * sigmaSteps;
+            var cutoffSteps = Math.Min(4 * sigmaSteps, gridSteps/2.0);
+
+            var phiGridPosition = GetPhiGridPosition(phi);
+            var psiGridPosition = GetPPsiGridPosition(psi);
+
+            var phiStart = (int)Math.Floor(phiGridPosition - cutoffSteps);
+            var phiEnd = (int) Math.Ceiling(phiGridPosition + cutoffSteps);
+            var psiStart = (int) Math.Floor(psiGridPosition - cutoffSteps);
+            var psiEnd = (int) Math.Ceiling(psiGridPosition + cutoffSteps);
+
+            var probabilityDensitySum = 0.0;
+            for (int psiIdx = psiStart; psiIdx <= psiEnd; psiIdx++)
+            {
+                int y;
+                if (psiIdx < 0)
+                    y = psiIdx + gridSteps;
+                else if (psiIdx >= gridSteps)
+                    y = psiIdx - gridSteps;
+                else
+                    y = psiIdx;
+                for (int phiIdx = phiStart; phiIdx <= phiEnd; phiIdx++)
+                {
+                    int x;
+                    if (phiIdx < 0)
+                        x = phiIdx + gridSteps;
+                    else if (phiIdx >= gridSteps)
+                        x = phiIdx - gridSteps;
+                    else
+                        x = phiIdx;
+                    var xDistance = x - phiGridPosition;
+                    var yDistance = y - psiGridPosition;
+                    var distanceFromPointSquared = xDistance * xDistance + yDistance * yDistance;
+                    var probabilityDensity = Math.Exp(-distanceFromPointSquared / (2* sigmaStepsSquared));
+                    distribution[y, x] += probabilityDensity;
+                    probabilityDensitySum += probabilityDensity;
+                }
+            }
+            return probabilityDensitySum;
         }
 
         private static Vector2D[,] GenerateGradientPlot(double[,] distributionPlot)
@@ -118,8 +208,8 @@ namespace ChemistryLibrary.Simulation
 
         public Vector2D GetGradient(UnitValue phi, UnitValue psi)
         {
-            var xIdx = (int)GetGridPositionFromAngle(phi);
-            var yIdx = (int)GetGridPositionFromAngle(psi);
+            var xIdx = (int)GetPhiGridPosition(phi);
+            var yIdx = (int)GetPPsiGridPosition(psi);
 
             return GradientPlot[xIdx, yIdx];
         }
@@ -134,9 +224,13 @@ namespace ChemistryLibrary.Simulation
             };
             return angles;
         }
-        private double GetGridPositionFromAngle(UnitValue phi)
+        private double GetPhiGridPosition(UnitValue phi)
         {
             return (phi.In(Unit.Degree) + 180) * gridSteps / 360;
+        }
+        private double GetPPsiGridPosition(UnitValue psi)
+        {
+            return gridSteps * (1-(psi.In(Unit.Degree) + 180) / 360);
         }
     }
 }
